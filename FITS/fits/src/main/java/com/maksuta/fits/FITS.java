dfbdbc27569381fc3cc20730f75db49fc2d837d6
jmaksuta <jmaksuta@colostate.edu>
2025-05-22 08:20:36 -0400

    Added stubs for ConformingExtension and SpecialRecord. Minor development code changes. Current progress.

--- FITS/fits/src/main/java/com/maksuta/fits/FITS.java
+++ FITS/fits/src/main/java/com/maksuta/fits/FITS.java
@@ -1,5 +1,13 @@
 package com.maksuta.fits;
 
+import java.util.LinkedList;
+
 public class FITS {
+
+    private LinkedList<FitsBlock> blocks;
+
+    public FitsBlock getHDU() {
+        return this.blocks.get(0);
+    }
     
 }
